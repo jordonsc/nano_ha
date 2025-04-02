eb10e81c28d622d2685be211e04b109613290c38
Jordon Scott <jordon.scott@zerohash.com>
2025-04-02 13:46:42 +1100

    Adding NL colour modes to integration

--- src/custom_components/nano_ha/light.py
+++ src/custom_components/nano_ha/light.py
@@ -9,7 +9,7 @@ import voluptuous as vol
 
 # Import the device class from the component that you want to support
 import homeassistant.helpers.config_validation as cv
-from homeassistant.components.light import (ATTR_BRIGHTNESS, PLATFORM_SCHEMA, LightEntity, ColorMode)
+from homeassistant.components.light import (ATTR_BRIGHTNESS, ATTR_HS_COLOR, ATTR_COLOR_TEMP_KELVIN, PLATFORM_SCHEMA, LightEntity, ColorMode)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
@@ -21,6 +21,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required("title"): cv.string,
     vol.Required("addr"): cv.string,
     vol.Required("token"): cv.string,
+    vol.Optional("brightness_only", default=False): cv.boolean,
 })
 
 
@@ -35,25 +36,37 @@ async def async_setup_platform(
     title = config["title"]
     addr = config["addr"]
     token = config["token"]
+    brightness_only = config["brightness_only"]
 
     # Add devices
-    add_entities([NanoLight(hass, title, addr, token)])
+    add_entities([NanoLight(hass, title, addr, token, brightness_only)])
 
 
 class NanoLight(LightEntity):
-    def __init__(self, hass, entity_name, addr, token) -> None:
+    def __init__(self, hass, entity_name, addr, token, brightness_only=False) -> None:
         self._hass = hass
         self._entity_name = entity_name
         self._nl: Nanoleaf|None = None
         self._addr = addr
         self._token = token
         self._name = "Nanoleaf Light"
+        self.brightness_only = brightness_only
+
+        self._attr_min_color_temp_kelvin = 2500
+        self._attr_max_color_temp_kelvin = 6500
 
         self._attr_is_on = False
         self._attr_brightness = 0
-        self._attr_color_mode = ColorMode.BRIGHTNESS
-        self._attr_supported_color_modes = {ColorMode.BRIGHTNESS}
+        self._attr_color_temp_kelvin = self._attr_max_color_temp_kelvin
+        self._attr_hs_color = (0, 0)
 
+        if self.brightness_only:
+            self._attr_color_mode = ColorMode.BRIGHTNESS
+            self._attr_supported_color_modes = {ColorMode.BRIGHTNESS}
+        else:
+            self._attr_color_mode = ColorMode.HS
+            self._attr_supported_color_modes = {ColorMode.COLOR_TEMP, ColorMode.HS}
+        
     @property
     def name(self) -> str:
         """Return the display name of this light."""
@@ -76,7 +89,17 @@ class NanoLight(LightEntity):
         """
         await self.init_nl()
         await self._hass.async_add_executor_job(self._nl.power_on)
-        await self._hass.async_add_executor_job(self._nl.set_brightness, round(kwargs.get(ATTR_BRIGHTNESS, 255) / 2.55))
+
+        if ATTR_BRIGHTNESS in kwargs:
+            await self._hass.async_add_executor_job(self._nl.set_brightness, round(kwargs.get(ATTR_BRIGHTNESS) / 2.55))
+
+        if ATTR_HS_COLOR in kwargs:
+            hue_sat = kwargs.get(ATTR_HS_COLOR)
+            await self._hass.async_add_executor_job(self._nl.set_hue, round(hue_sat[0]))
+            await self._hass.async_add_executor_job(self._nl.set_saturation, round(hue_sat[1]))
+        elif ATTR_COLOR_TEMP_KELVIN in kwargs:
+            await self._hass.async_add_executor_job(self._nl.set_color_temp, kwargs.get(ATTR_COLOR_TEMP_KELVIN))
+
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """
@@ -93,3 +116,21 @@ class NanoLight(LightEntity):
         self._name = await self._hass.async_add_executor_job(self._nl.get_name)
         self._attr_is_on = await self._hass.async_add_executor_job(self._nl.get_power)
         self._attr_brightness = round(await self._hass.async_add_executor_job(self._nl.get_brightness) * 2.55)
+
+        if self.brightness_only:
+            return
+
+        colour_mode = await self._hass.async_add_executor_job(self._nl.get_color_mode)
+        if colour_mode == "ct":
+            # Color temperature
+            self._attr_color_mode = ColorMode.COLOR_TEMP
+            self._attr_color_temp_kelvin = await self._hass.async_add_executor_job(self._nl.get_color_temp)
+
+        else:
+            # "hs" for hue-saturation, but we'll also include "effect" here
+            self._attr_color_mode = ColorMode.HS
+
+        # Always update hue/sat - even if in color temp mode
+        hue = await self._hass.async_add_executor_job(self._nl.get_hue)
+        sat = await self._hass.async_add_executor_job(self._nl.get_saturation)
+        self._attr_hs_color = (hue, sat)
